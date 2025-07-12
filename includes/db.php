87f573001ee466adf8065f12addef15f319ec54a
ILYAS _ RAMADHAN <ramadhanilyas71@gmail.com>
2025-07-12 11:38:33 +0700

    db.php

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,11 @@
 <?php
-$host = getenv("DB_HOST") ?: "localhost";
-$user = getenv("DB_USER") ?: "root";
-$pass = getenv("DB_PASS") ?: "";
-$db   = getenv("DB_NAME") ?: "checklist_toilet";
+$host = "metro.proxy.rlwy.net";
+$port = 56793;
+$user = "root";
+$pass = "KFRuhiVOImdekJHBZNfqLCPNSxGSjitG";
+$db   = "railway";
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);

a042cfc916529ce6195f3035700f88007d459f15
ILYAS _ RAMADHAN <ramadhanilyas71@gmail.com>
2025-07-12 11:30:07 +0700

    db.php

--- includes/db.php
+++ includes/db.php
@@ -1,10 +1,11 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "checklist_toilet";
+$host = getenv("DB_HOST") ?: "localhost";
+$user = getenv("DB_USER") ?: "root";
+$pass = getenv("DB_PASS") ?: "";
+$db   = getenv("DB_NAME") ?: "checklist_toilet";
 
-$conn = new mysqli("localhost", "root", "", "checklist_toilet");
+$conn = new mysqli($host, $user, $pass, $db);
+
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
